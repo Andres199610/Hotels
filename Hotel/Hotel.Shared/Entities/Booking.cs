d84f552bf22f02bf25e6f1f7949ae86ea9226716
ANDRES FELIPE RAMIREZ USUGA <andresramirez229848@correo.itm.edu.co>
2023-10-02 20:17:59 -0500

    Creation Entities

---
+++ Hotel/Hotel.Shared/Entities/Booking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Shared.Entities
+{
+   public class Booking
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Reserva")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null!;
+
+        public int amount_Adults { get; set; }
+
+        public int amount_Children { get; set; }
+
+        public int amount_Baby { get; set; }
+
+    }
+}
---
+++ Hotel/Hotel.Shared/Entities/RoomImage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Shared.Entities
+{
+    public class RoomImage
+    {
+
+        public int Id { get; set; }
+
+        public Room Room { get; set; } = null!;
+
+        public int RoomId { get; set; }
+
+        [Display(Name = "Imagen")]
+        public string Image { get; set; } = null!;
+
+    }
+}
