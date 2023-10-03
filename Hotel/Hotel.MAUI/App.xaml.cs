5c0db9d39e8fa697baa8ae7b9248e283e6953104
ANDRES FELIPE RAMIREZ USUGA <andresramirez229848@correo.itm.edu.co>
2023-10-02 20:06:12 -0500

    Version Inicial

---
+++ Hotel/Hotel.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Hotel.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Hotel/Hotel.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Hotel.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
