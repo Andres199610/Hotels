cb323de67d9ca30141ade72c6c8ef22bf5761935
ANDRES FELIPE RAMIREZ USUGA <andresramirez229848@correo.itm.edu.co>
2023-10-04 11:52:35 -0500

    Create IRepository

--- Hotel/Hotel.WEB/Program.cs
+++ Hotel/Hotel.WEB/Program.cs
@@ -1,12 +1,13 @@
 using Hotel.WEB;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Sales.WEB.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7216") });
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddScoped<IRepository, Repository>();
 await builder.Build().RunAsync();
